ed2d024b8674a098ac71cd95a06ba92837e6177c
Redxjak <Redxjak@users.noreply.github.com>
2026-05-28 21:31:21 +0900

    Update Tales of Visteria version to 0.8.2

--- ToV/assets/app.js
+++ ToV/assets/app.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  const VERSION = "0.8.1";
+  const VERSION = "0.8.2";
   const MAP_DIRECTIONS = ["LEFT", "UP", "RIGHT", "DOWN"];
   const BASE_LEVEL = 5;
   const BASE_XP_TO_NEXT = 100;
